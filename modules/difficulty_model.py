e60369b2ba5ca94e4fd9c0266d2119fd607e7b1d
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2024-12-26 23:53:43 +0530

    added question generation pipeline

--- modules/difficulty_model.py
+++ modules/difficulty_model.py
@@ -1,46 +1,87 @@
 class DifficultyModel:
     def __init__(self):
         self.category_weight = {
-            "Arithmetic Sequences" : 4,
-            "Geometric Sequences" : 14,
-            "Mixed Sequences" : 15,
-            "Prime Numbers" : 12,
-            "Factorisation Problems" : 6,
-            "Odd One Out" : 1,
-            "Number Puzzle" : 3,
-            "Time and Work Problems" : 9,
-            "BODMAS" : 5,
-            "Algebra" : 10,
-            "Profit and Loss" : 8,
-            "Probability and Statistics" : 13,
-            "Currency and Exchange" : 7,
-            "Geometry" : 11,
-            "Calendar Problems" : 2,
+            "Arithmetic Sequences": 4,
+            "Geometric Sequences": 14,
+            "Mixed Sequences": 15,
+            "Prime Numbers": 12,
+            "Factorisation Problems": 6,
+            "Odd One Out": 1,
+            "Number Puzzle": 3,
+            "Time and Work Problems": 9,
+            "BODMAS": 5,
+            "Algebra": 10,
+            "Profit and Loss": 8,
+            "Probability and Statistics": 13,
+            "Currency and Exchange": 7,
+            "Area and Perimeter": 11,
+            "Volumne": 11,
+            "Angles":11,
+            "Triangles":11,
+            "Circles":11,
+            "Quadrilaterals":11,
+            "Spatial Reasoning":11,
+            "Calendar Problems": 2,
         }
 
-
+    def is_prime(self, n):
+        if n < 2:
+            return False
+        
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+            
+        return True
+    
+    def analyze_numbers(self, numbers):
+        score = 0
+        
+        for num in numbers:  
+            if num > 30:
+                score += 1
+            
+            if num > 70:
+                score += 2
+                
+            if num != 0 and num not in [1, 2, 5, 10, 25, 50, 100]:
+            	if num % 2 != 0 and num % 5 != 0:
+            		score += 1
+            
+            # Prime numbers are typically harder
+            if self.is_prime(int(num)):
+                score += 2
+        
+        return score
+    
+    
     def calculate_score(self, resp, category):
         try:
             topic_complexity_weight = self.category_weight[category]
-
+            
             num_vars = len(resp['variable_relations']['variables'])
             num_steps = len(resp['solution'].split("."))
             rel = list(resp['variable_relations']['relations'].values())
-
+            
             num_mul_or_division_steps = 0
             num_add_or_subtract_steps = 0
-
+            
             for i in rel:
                 if '+' in i or '-' in i:
                     num_add_or_subtract_steps += 1
-                    
-                if '/' in i or '*' in i :
-                    num_add_or_subtract_steps += 1
+                if '/' in i or '*' in i:
+                    num_mul_or_division_steps += 1
             
-            diff_score = 2.5*topic_complexity_weight + 1.5*num_vars + 0.5*num_steps + 0.5*num_add_or_subtract_steps + num_mul_or_division_steps
-
+            diff_score = 2.5 * topic_complexity_weight + 1.5 * num_vars + 0.5 * num_steps + 0.5 * num_add_or_subtract_steps + num_mul_or_division_steps
+            
             return diff_score
-        
+               
         except:
             print(f"{category} not in defined category")
             raise TypeError("Program exit with code -1")
+        
+        
+    def update_score(self, values, template_difficulty):
+        numerical_complexity = self.analyze_numbers(values.values())
+
+        return template_difficulty+numerical_complexity
--- modules/questions.py
+++ modules/questions.py
@@ -1,6 +1,9 @@
 from datasets import load_dataset
 import random
 import pandas as pd
+import math
+import re
+from lib.utils import find_independent_vars
 
 
 class RandomDataset:
@@ -13,3 +16,74 @@ class RandomDataset:
         sample = self.data.sample(num, random_state=n)
 
         return sample['question'].values.tolist()
+    
+
+class QuestionModel:
+    def __init__(self, max_value=50):
+        self.max_val = max_value
+    
+    def _replace_target_var(self, string):
+        target_pattern = r"\\boxed\{\{(.*?)\}\}"
+        target_res = re.search(target_pattern, string)
+        
+        span = target_res.span()
+        target = target_res.group(1)
+        res = string[:span[0]] + "{" + f"{target}" + "}" + string[span[1]:]
+        
+        return res
+
+    def _generate_independent_vars(self, variables, constraints, values, index=0):
+        if len(variables) == len(values) or index >= len(variables):
+            return values
+        
+        var_name = variables[index]
+        min_val = constraints[var_name][0]
+        max_val = constraints[var_name][1]
+
+        if max_val == math.inf:
+            max_val = self.max_val
+        else:
+            ind = variables.index(max_val)
+            values = self._generate_independent_vars(variables, constraints, values, ind)
+            max_val = values[max_val]
+
+        values[var_name] = random.randint(min_val, max_val)
+
+        return self._generate_independent_vars(variables, constraints, values, index+1)
+
+    def check_constraints(self, values):
+        is_int = all([isinstance(i, int) for i in values.values()])
+        is_positive = all([i > 0 for i in values.values()])
+
+        if not is_int or not is_positive:
+            return False
+        
+        return True
+
+    def generate_values(self, variables):
+        var = variables['variables']
+        relations = variables['relations']
+        constraints = variables['constraints']
+
+        independent_var = find_independent_vars(var, relations)
+        values = self._generate_independent_vars(independent_var, constraints, {})
+
+        for i in var:
+            if i not in values:
+                values[i] = eval(relations[i].format(**values))
+
+        if self.check_constraints(values):
+            return values
+        
+        return self.generate_values(variables)
+
+    def generate_question(self, question):
+        values = self.generate_values(question['variable_relations'])
+        sol = self._replace_target_var(question['solution'])
+
+        res = {
+            "question": question['question'].format(**values),
+            "solution": sol.format(**values)
+        }
+
+        return res
